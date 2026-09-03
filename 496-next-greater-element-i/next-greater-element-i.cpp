3a0ed111af2a3597426725f87b2d3df4cd32653c
anujnath4512 <anujnath4512@gmail.com>
2026-09-04 02:02:27 +0530

    Time: 3 ms (41.18%) | Memory: 12.2 MB (92.54%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> ans;
+
+        for(int i = 0; i < nums1.size(); i++) {
+            
+            int x = nums1[i];
+            int greater = -1;
+
+            // Find x inside nums2
+            for(int j = 0; j < nums2.size(); j++) {
+                
+                if(nums2[j] == x) {
+                    
+                    // Now look to the right of x
+                    for(int k = j + 1; k < nums2.size(); k++) {
+                        
+                        if(nums2[k] > x) {
+                            greater = nums2[k];
+                            break;
+                        }
+                    }
+                    
+                    break;
+                }
+            }
+        ans.push_back(greater);
+        }
+    return ans;
+    }
+};
