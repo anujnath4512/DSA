4affa789bbb6d728dc86ccdf02c5280b13c1e3a2
anujnath4512 <anujnath4512@gmail.com>
2026-09-20 03:04:54 +0530

    Time: 4 ms (47.97%) | Memory: 24.4 MB (92.51%) - LeetSync

---
+++ 859-design-circular-deque/design-circular-deque.cpp
@@ -0,0 +1,88 @@
+
+class MyCircularDeque {
+private:
+    vector<int> arr;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+public:
+    MyCircularDeque(int k) {
+        capacity = k;
+        arr.resize(k);
+        front = 0;
+        rear = 0;
+        size = 0;
+    }
+
+    bool insertFront(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        front = (front - 1 + capacity) % capacity;
+        arr[front] = value;
+        size++;
+
+        return true;
+    }
+
+    bool insertLast(int value) {
+        if (isFull()) {
+            return false;
+        }
+
+        arr[rear] = value;
+        rear = (rear + 1) % capacity;
+        size++;
+
+        return true;
+    }
+
+    bool deleteFront() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        front = (front + 1) % capacity;
+        size--;
+
+        return true;
+    }
+
+    bool deleteLast() {
+        if (isEmpty()) {
+            return false;
+        }
+
+        rear = (rear - 1 + capacity) % capacity;
+        size--;
+
+        return true;
+    }
+
+    int getFront() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[front];
+    }
+
+    int getRear() {
+        if (isEmpty()) {
+            return -1;
+        }
+
+        return arr[(rear - 1 + capacity) % capacity];
+    }
+
+    bool isEmpty() {
+        return size == 0;
+    }
+
+    bool isFull() {
+        return size == capacity;
+    }
+};
